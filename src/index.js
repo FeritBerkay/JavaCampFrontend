f2065605ebb59c8e49b1b11c5393851395825c49
FeritBerkay <76204009+FeritBerkay@users.noreply.github.com>
2021-06-06 01:23:09 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//sanal bir html yapısı oluşturup. app componentini roota yerleştirdik.
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
